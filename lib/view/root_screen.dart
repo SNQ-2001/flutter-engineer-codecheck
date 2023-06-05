206755dad9014506c37220cb320ba0e4328d0972
SNQ-2001 <info@taishin-miyamoto.com>
2023-06-06 00:31:41 +0900

    clean: コードを整理

--- lib/view/root_screen.dart
+++ lib/view/root_screen.dart
@@ -11,21 +11,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final apiClient = APIClient();
+  final _apiClient = APIClient();
 
-  Future<void> fetchSearchRepository() async {
-    try {
-      final exampleData = await apiClient.getSearchRepository("flutter");
-      final totalCount = exampleData.totalCount;
-      print('$totalCount');
-    } catch (e) {
-      print('Error: $e');
-    }
+  bool _searchBoolean = false;
+
+  var list = ["メッセージ", "メッセージ", "メッセージ", "メッセージ", "メッセージ",];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: !_searchBoolean ?
+        Text(widget.title, style: const TextStyle(color: Colors.white)) :
+        Padding(padding: const EdgeInsets.all(5), child: _searchTextField()),
+          actions: !_searchBoolean ? [_searchIcon()] : [_clearIcon()]
+      ),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          if (index >= list.length) {
+            list.addAll(["メッセージ","メッセージ","メッセージ","メッセージ",]);
+          }
+          return _githubItem(list[index]);
+        },
+      ),
+    );
   }
 
   Widget _searchTextField() {
     return TextField(
-      onSubmitted: (String value) { print("$value"); },
+      onSubmitted: (String value) { fetchSearchRepository(value); },
       autofocus: true,
       cursorColor: Colors.white,
       style: const TextStyle(
@@ -49,55 +64,44 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  var list = ["メッセージ", "メッセージ", "メッセージ", "メッセージ", "メッセージ",];
-
-  bool _searchBoolean = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: !_searchBoolean ?
-        Text(widget.title, style: const TextStyle(color: Colors.white)) :
-        Padding(padding: const EdgeInsets.all(5), child: _searchTextField()),
-          actions: !_searchBoolean ? [
-            IconButton(icon: const Icon(Icons.search, color: Colors.white), onPressed: () {
-              setState(() {
-                _searchBoolean = true;
-              });
-            })
-          ] : [
-            IconButton(icon: const Icon(Icons.clear, color: Colors.white), onPressed: () {
-              setState(() {
-                _searchBoolean = false;
-              });
-            })
-          ]
-      ),
-      body: ListView.builder(
-        itemBuilder: (BuildContext context, int index) {
-          if (index >= list.length) {
-            list.addAll(["メッセージ","メッセージ","メッセージ","メッセージ",]);
-          }
-          return _messageItem(list[index]);
-        },
-      ),
-    );
+  Widget _searchIcon() {
+    return IconButton(icon: const Icon(Icons.search, color: Colors.white), onPressed: () {
+      setState(() {
+        _searchBoolean = true;
+      });
+    });
   }
 
-  Widget _messageItem(String title) {
+  Widget _clearIcon() {
+    return IconButton(icon: const Icon(Icons.clear, color: Colors.white), onPressed: () {
+      setState(() {
+        _searchBoolean = false;
+      });
+    });
+  }
+
+  Widget _githubItem(String title) {
     return Container(
       decoration: const BoxDecoration(border: Border(bottom: BorderSide(width: 1.0, color: Colors.grey))),
       child: ListTile(
         title: Text(
           title,
-          style: TextStyle(color: Colors.black, fontSize: 18.0),
+          style: const TextStyle(color: Colors.black, fontSize: 18.0),
         ),
         onTap: () {
           print("onTap called.");
         },
       ),
     );
+  }
+
+  Future<void> fetchSearchRepository(String q) async {
+    try {
+      final exampleData = await _apiClient.getSearchRepository(q);
+      final totalCount = exampleData.totalCount;
+      print('$totalCount');
+    } catch (e) {
+      print('Error: $e');
+    }
   }
 }
